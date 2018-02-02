d1570e792fc6f418db46f5c68fc9e0db08f16a5a
Peter <ptk726@gmail.com>
2018-02-02 17:56:25 -0500

    added authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #   :include ActionController::RequestForgeryProtection
+#   protect_from_forgery with: :exception
+#   helper_method :current_user, :logged_in, :authorize
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in
+    !!current_user
+  end
+
+  def authorize
+    redirect_to login_path unless logged_in
+  end
+  
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:nickname, :name, :username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
---
+++ config/initializers/devise.rb
@@ -0,0 +1,4 @@
+Devise.setup do |config|
+  config.secret_key = 'c37cada2690492151e2b4cbdfcc5e926ebf0db3cdf02c7160c615ad461117e31e58b3ce1af2b1a39fcd1883d9ffd5cc83a3b4c7fa98c7869450ad40450122568'
+end
+
