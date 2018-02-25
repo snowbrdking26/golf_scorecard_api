287efa79553142a0c50416584590e9e1420e2075
Peter <ptk726@gmail.com>
2018-02-25 18:50:16 -0500

    update minimum length of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:name, :nickname, :email, password_length: 1]
+          :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:name, :nickname, :email, password_length: 1..128]
   include DeviseTokenAuth::Concerns::User
 end
