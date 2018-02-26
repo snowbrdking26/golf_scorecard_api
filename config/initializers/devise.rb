1277ec49d5ba7baffa273955d847a84c72b50e4d
Peter <ptk726@gmail.com>
2018-02-25 19:02:37 -0500

    update minimum length of username

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,6 @@
 Devise.setup do |config|
   config.secret_key = 'c37cada2690492151e2b4cbdfcc5e926ebf0db3cdf02c7160c615ad461117e31e58b3ce1af2b1a39fcd1883d9ffd5cc83a3b4c7fa98c7869450ad40450122568'
 
-  config.password_length = 3..128
+  config.password_length = 1..128
 end
 
